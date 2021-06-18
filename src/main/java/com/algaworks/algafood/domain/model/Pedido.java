15252332aa38c4f32b76103b9ee678dd7e06f327
Antonio Pires <tonan@globo.com>
2021-06-18 18:33:37 -0300

    7.12. Desafio: Criando migrações e mapeando as entidades Pedido e ItemPedido - parte 2 concluido

--- src/main/java/com/algaworks/algafood/domain/model/Pedido.java
+++ src/main/java/com/algaworks/algafood/domain/model/Pedido.java
@@ -2,17 +2,18 @@ package com.algaworks.algafood.domain.model;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Column;
-import javax.persistence.Embeddable;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 import org.hibernate.annotations.CreationTimestamp;
 
@@ -69,6 +70,7 @@ public class Pedido {
 	@JoinColumn(name = "usuario_cliente_id" , nullable = false)
 	private Usuario cliente;
 	
+	@OneToMany(mappedBy = "pedido")
 	private List<ItemPedido> itens = new ArrayList<>();
 	
 
