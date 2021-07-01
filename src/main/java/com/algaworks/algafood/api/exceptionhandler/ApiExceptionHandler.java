92af58087478c26d22eef783163406d265c3ad90
Antonio Pires <tonan@globo.com>
2021-07-01 16:00:24 -0300

    8.18. Padronizando o formato de problemas no corpo de respostas com a RFC 7807

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,7 +1,5 @@
 package com.algaworks.algafood.api.exceptionhandler;
 
-import java.time.LocalDateTime;
-
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,28 +17,51 @@ import com.algaworks.algafood.domain.exception.NegocioException;
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 	
 	@ExceptionHandler(EntidadeNaoEncontradaException.class)
-	public ResponseEntity<?> tratarEntidadeNaoEncontradaException(
+	public ResponseEntity<?> handleEntidadeNaoEncontradaException(
 			EntidadeNaoEncontradaException ex, WebRequest request) {
 		
-		return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(),
-				HttpStatus.NOT_FOUND, request);
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		ProblemType problemType = ProblemType.ENTIDADE_NAO_ENCONTRADA;
+		String detail = ex.getMessage();
+		
+		Problem problem = createProblemBuilder(status, problemType, detail).build();
+		
+//		Problem problem = Problem.builder()
+//				.status(status.value())
+//				.type("https://algafood.com.br/entidade-nao-encontrada")
+//				.title("Entidade não encontrada")
+//				.detail(ex.getMessage())
+//				.build();
+		
+		return handleExceptionInternal(ex, problem, new HttpHeaders(),
+				status, request);
 
 	}
 	
 	@ExceptionHandler(EntidadeEmUsoException.class)
-	public ResponseEntity<?> tratarEntidadeEmUsoException(
+	public ResponseEntity<?> handleEntidadeEmUsoException(
 			EntidadeEmUsoException ex, WebRequest request) {
+		HttpStatus status = HttpStatus.CONFLICT;
+		ProblemType problemType = ProblemType.ENTIDADE_EM_USO;
+		String detail = ex.getMessage();
 		
-		return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(),
-				HttpStatus.CONFLICT, request);
+		Problem problem = createProblemBuilder(status, problemType, detail).build();
+		
+		return handleExceptionInternal(ex, problem, new HttpHeaders(),
+				status, request);
 	}
 	
 	@ExceptionHandler(NegocioException.class)
-	public ResponseEntity<?> tratarNegocioException(
+	public ResponseEntity<?> handleNegocioException(
 			NegocioException ex, WebRequest request) {
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		ProblemType problemType = ProblemType.ERRO_NEGOCIO;
+		String detail = ex.getMessage();
 		
-		return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(),
-				HttpStatus.BAD_REQUEST, request);
+		Problem problem = createProblemBuilder(status, problemType, detail).build();
+		
+		return handleExceptionInternal(ex, problem, new HttpHeaders(),
+				status, request);
 	}
 	
 	@Override
@@ -48,18 +69,29 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 			HttpStatus status, WebRequest request) {
 		
 		if (body == null) {		
-			body = Problema.builder()
-					.dataHora(LocalDateTime.now())
-					.mensagem(status.getReasonPhrase())
+			body = Problem.builder()
+					.title(status.getReasonPhrase())
+					.status(status.value())
 					.build();
 		} else if (body instanceof String) {
-			body = Problema.builder()
-					.dataHora(LocalDateTime.now())
-					.mensagem((String) body)
+			body = Problem.builder()
+					.title((String) body)
+					.status(status.value())
 					.build();
 		}
 		
 		return super.handleExceptionInternal(ex, body, headers, status, request);
+	}
+	
+	private Problem.ProblemBuilder createProblemBuilder(HttpStatus status,
+			ProblemType problemType, String detail) {
+		
+		return Problem.builder()
+				.status(status.value())
+				.type(problemType.getUri())
+				.title(problemType.getTitle())
+				.detail(detail);
+				
 	}
 	
 }
