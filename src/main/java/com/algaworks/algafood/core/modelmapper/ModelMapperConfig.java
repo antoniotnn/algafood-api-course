435009834959991c5abebf8a1a11c5a9a833a070
Antonio Pires <tonan@globo.com>
2021-07-26 15:42:23 -0300

    11.19. Estratégias de nomes de propriedades: snake case vs lower camel case

--- src/main/java/com/algaworks/algafood/core/modelmapper/ModelMapperConfig.java
+++ src/main/java/com/algaworks/algafood/core/modelmapper/ModelMapperConfig.java
@@ -4,9 +4,6 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.algaworks.algafood.api.model.RestauranteModel;
-import com.algaworks.algafood.domain.model.Restaurante;
-
 @Configuration
 public class ModelMapperConfig {
 	
