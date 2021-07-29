6037c1bb075af91037fca8d4c7ee9b676f893eea
Antonio Pires <tonan@globo.com>
2021-07-29 18:09:54 -0300

    12.8. Desafio: implementando os endpoints de grupos

--- src/main/java/com/algaworks/algafood/domain/service/CadastroFormaPagamentoService.java
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroFormaPagamentoService.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
-import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.exception.FormaPagamentoNaoEncontradaException;
 import com.algaworks.algafood.domain.model.FormaPagamento;
 import com.algaworks.algafood.domain.repository.FormaPagamentoRepository;
