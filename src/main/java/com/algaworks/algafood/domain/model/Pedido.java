cbf86ad60d03ff40eec7e3cfa8b27ca7b86260a9
Antonio Pires <tonan@globo.com>
2021-08-06 07:23:45 -0300

    12.19. Desafio: Implementando os endpoints de consulta de pedidos parte 1

--- src/main/java/com/algaworks/algafood/domain/model/Pedido.java
+++ src/main/java/com/algaworks/algafood/domain/model/Pedido.java
@@ -8,6 +8,8 @@ import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -56,7 +58,8 @@ public class Pedido {
 	private Endereco enderecoEntrega;
 	
 	@Column
-	private StatusPedido status;
+	@Enumerated(EnumType.STRING)
+	private StatusPedido status = StatusPedido.CRIADO;
 	
 	@ManyToOne
 	@JoinColumn(nullable = false)
@@ -73,7 +76,19 @@ public class Pedido {
 	@OneToMany(mappedBy = "pedido")
 	private List<ItemPedido> itens = new ArrayList<>();
 	
-
+	public void calcularValorTotal() {
+		this.subtotal = getItens().stream()
+				.map(item -> item.getPrecoTotal())
+				.reduce(BigDecimal.ZERO, BigDecimal::add);
+		this.valorTotal = this.subtotal.add(this.taxaFrete);
+	}
 	
+	public void definirFrete() {
+		setTaxaFrete(getRestaurante().getTaxaFrete());
+	}
+	
+	public void atribuirPedidoAosItens() {
+		getItens().forEach(item -> item.setPedido(this));
+	}
 	
 }
