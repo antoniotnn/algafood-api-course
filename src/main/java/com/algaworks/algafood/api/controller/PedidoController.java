613fac1dedd4eac3e66c71bc78667971ed30bb84
Antonio Pires <tonan@globo.com>
2021-08-09 21:30:35 -0300

    12.21. Desafio: Implementando o endpoint de emissão de pedidos

--- src/main/java/com/algaworks/algafood/api/controller/PedidoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/PedidoController.java
@@ -2,16 +2,28 @@ package com.algaworks.algafood.api.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.algaworks.algafood.api.assembler.PedidoInputDisassembler;
 import com.algaworks.algafood.api.assembler.PedidoModelAssembler;
 import com.algaworks.algafood.api.assembler.PedidoResumoModelAssembler;
 import com.algaworks.algafood.api.model.PedidoModel;
 import com.algaworks.algafood.api.model.PedidoResumoModel;
+import com.algaworks.algafood.api.model.input.PedidoInput;
+import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
+import com.algaworks.algafood.domain.exception.NegocioException;
+import com.algaworks.algafood.domain.model.Pedido;
+import com.algaworks.algafood.domain.model.Usuario;
 import com.algaworks.algafood.domain.repository.PedidoRepository;
 import com.algaworks.algafood.domain.service.EmissaoPedidoService;
 
@@ -31,6 +43,9 @@ public class PedidoController {
 	@Autowired
 	private PedidoResumoModelAssembler pedidoResumoModelAssembler;
 	
+	@Autowired
+	private PedidoInputDisassembler pedidoInputDisassembler;
+	
 	
 	@GetMapping
 	public List<PedidoResumoModel> listar(){
@@ -40,6 +55,28 @@ public class PedidoController {
 	@GetMapping("/{pedidoId}")
 	public PedidoModel buscar(@PathVariable Long pedidoId) {
 		return pedidoModelAssembler.toModel(emissaoPedidoService.buscarOuFalhar(pedidoId));
+	}
+	
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public PedidoModel adicionar(@RequestBody @Valid PedidoInput pedidoInput) {
+		
+		try {
+			
+			Pedido pedido = pedidoInputDisassembler.toDomainObject(pedidoInput);
+	
+			//TODO pegar usuário autenticado
+			pedido.setCliente(new Usuario());
+			pedido.getCliente().setId(1L);
+			
+			pedido = emissaoPedidoService.emitir(pedido);
+			
+			return pedidoModelAssembler.toModel(pedido);
+			
+		} catch (EntidadeNaoEncontradaException e) {
+			throw new NegocioException(e.getMessage(), e);
+		}
 	}
 	
 }
--- src/main/java/com/algaworks/algafood/domain/service/EmissaoPedidoService.java
+++ src/main/java/com/algaworks/algafood/domain/service/EmissaoPedidoService.java
@@ -2,9 +2,16 @@ package com.algaworks.algafood.domain.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.algaworks.algafood.domain.exception.NegocioException;
 import com.algaworks.algafood.domain.exception.PedidoNaoEncontradoException;
+import com.algaworks.algafood.domain.model.Cidade;
+import com.algaworks.algafood.domain.model.FormaPagamento;
 import com.algaworks.algafood.domain.model.Pedido;
+import com.algaworks.algafood.domain.model.Produto;
+import com.algaworks.algafood.domain.model.Restaurante;
+import com.algaworks.algafood.domain.model.Usuario;
 import com.algaworks.algafood.domain.repository.PedidoRepository;
 
 @Service
@@ -13,10 +20,68 @@ public class EmissaoPedidoService {
 	@Autowired
 	private PedidoRepository pedidoRepository;
 	
+	@Autowired
+	private CadastroRestauranteService cadastroRestauranteService;
+	
+	@Autowired
+	private CadastroCidadeService cadastroCidadeService;
+	
+	@Autowired
+	private CadastroUsuarioService cadastroUsuarioService;
+	
+	@Autowired
+	private CadastroProdutoService cadastroProdutoService;
+	
+	@Autowired
+	private CadastroFormaPagamentoService cadastroFormaPagamenetoService;
+	
 	
 	public Pedido buscarOuFalhar(Long pedidoId) {
 		return pedidoRepository.findById(pedidoId)
 				.orElseThrow(()-> new PedidoNaoEncontradoException(pedidoId));
 	}
+	
+	@Transactional
+	public Pedido emitir(Pedido pedido) {
+		validarPedido(pedido);
+		validarItens(pedido);
+		
+		pedido.setTaxaFrete(pedido.getRestaurante().getTaxaFrete());
+		pedido.calcularValorTotal();
+		
+		return pedidoRepository.save(pedido);
+	}
+	
+	public void validarPedido(Pedido pedido) {
+		Cidade cidade = cadastroCidadeService.buscarOufalhar(pedido.getEnderecoEntrega().getCidade().getId());
+		Usuario cliente = cadastroUsuarioService.buscarOuFalhar(pedido.getCliente().getId());
+		Restaurante restaurante = cadastroRestauranteService.buscarOuFalhar(pedido.getRestaurante().getId());
+		FormaPagamento formaPagamento = cadastroFormaPagamenetoService.buscarOuFalhar(pedido.getFormaPagamento().getId());
+		
+		pedido.getEnderecoEntrega().setCidade(cidade);
+		pedido.setCliente(cliente);
+		pedido.setRestaurante(restaurante);
+		pedido.setFormaPagamento(formaPagamento);
+		
+		if (restaurante.naoAceitaFormaPagamento(formaPagamento)) {
+			throw new NegocioException(String.format("Forma de pagamento '%s' não é aceita por esse restairamte.", formaPagamento.getDescricao()));
+		}
+		
+		
+	}
+	
+	public void validarItens(Pedido pedido) {
+		pedido.getItens().forEach(item -> {
+			Produto produto = cadastroProdutoService.buscarOuFalhar(
+					pedido.getRestaurante().getId(), item.getProduto().getId());
+			
+			item.setPedido(pedido);
+			item.setProduto(produto);
+			item.setPrecoUnitario(produto.getPreco());
+		
+		});
+	}
+	
+	
 	
 }
