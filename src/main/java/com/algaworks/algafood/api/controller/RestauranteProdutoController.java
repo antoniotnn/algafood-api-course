611753a588ac5c1d4e7a27066436b71f135b1755
Antonio Pires <tonan@globo.com>
2021-08-16 06:38:25 -0300

    13.4. Implementando pesquisas simples na API

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteProdutoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteProdutoController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -45,10 +46,13 @@ public class RestauranteProdutoController {
 	private ProdutoInputDisassembler produtoInputDisassembler;
 
 	@GetMapping
-	public List<ProdutoModel> listar(@PathVariable Long restauranteId) {
+	public List<ProdutoModel> listar(@PathVariable Long restauranteId, @RequestParam(required = false) boolean incluirInativos) {
 		Restaurante restaurante = cadastroRestauranteService.buscarOuFalhar(restauranteId);
 		
-		return produtoModelAssembler.toCollectionModel(produtoRepository.findByRestaurante(restaurante));	
+		if (incluirInativos) {
+			return produtoModelAssembler.toCollectionModel(produtoRepository.findTodosByRestaurante(restaurante));
+		}
+		return produtoModelAssembler.toCollectionModel(produtoRepository.findAtivosByRestaurante(restaurante));
 	}
 
 	@GetMapping("/{produtoId}")
