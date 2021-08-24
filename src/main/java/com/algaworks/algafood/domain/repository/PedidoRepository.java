36c741a8c998d8dc5fb4d71489833a4e803a4175
Antonio Pires <tonan@globo.com>
2021-08-24 17:43:30 -0300

    13.20. Preenchendo um relatório JasperReports com JavaBeans e gerando bytes do PDF

--- src/main/java/com/algaworks/algafood/domain/repository/PedidoRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/PedidoRepository.java
@@ -7,9 +7,7 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import com.algaworks.algafood.domain.filter.VendaDiariaFilter;
 import com.algaworks.algafood.domain.model.Pedido;
-import com.algaworks.algafood.domain.model.dto.VendaDiaria;
 
 @Repository
 public interface PedidoRepository extends CustomJpaRepository<Pedido, Long>, 
