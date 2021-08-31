60ca016b5cf90876da83d92a4fef9cc53e80c8f9
Antonio Pires <tonan@globo.com>
2021-08-31 16:13:15 -0300

    14.24. Desafio: Implementando a exclusão de objetos do bucket da Amazon S3

--- src/main/java/com/algaworks/algafood/infrastructure/service/storage/S3FotoStorageService.java
+++ src/main/java/com/algaworks/algafood/infrastructure/service/storage/S3FotoStorageService.java
@@ -7,8 +7,11 @@ import org.springframework.stereotype.Service;
 
 import com.algaworks.algafood.core.storage.StorageProperties;
 import com.algaworks.algafood.domain.service.FotoStorageService;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.SdkClientException;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 
@@ -30,7 +33,7 @@ public class S3FotoStorageService implements FotoStorageService {
 	@Override
 	public void armazenar(NovaFoto novaFoto) {
 		try {
-			String caminhoArquivo = getCaminho(novaFoto.getNomeArquivo());
+			String caminhoArquivo = getCaminhoArquivo(novaFoto.getNomeArquivo());
 			
 			var objectMetadata = new ObjectMetadata();
 			objectMetadata.setContentType(novaFoto.getContentType());
@@ -48,16 +51,24 @@ public class S3FotoStorageService implements FotoStorageService {
 		}
 	}
 
-	private String getCaminho(String nomeArquivo) {
+	private String getCaminhoArquivo(String nomeArquivo) {
 		return String.format("%s/%s", storageProperties.getS3().getDiretorioFotos(), nomeArquivo);
 	}
 
 	@Override
 	public void remover(String nomeArquivo) {
-		// TODO Auto-generated method stub
+		
+		try {
+			String caminhoArquivo = getCaminhoArquivo(nomeArquivo);
+			
+			var deleteObjectRequest = new DeleteObjectRequest(
+					storageProperties.getS3().getBucket(), caminhoArquivo);
+			
+			amazonS3.deleteObject(deleteObjectRequest);
+		} catch (Exception e) {
+			throw new StorageException("Não foi possível excluir arquivo na Amazon S3", e);
+		} 
 		
 	}
 	
-
-
 }
