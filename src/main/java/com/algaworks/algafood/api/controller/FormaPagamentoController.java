0ca920e1f23d416955dab168a1eadd86807d7150
Antonio Pires <tonan@globo.com>
2021-09-14 19:24:15 -0300

    17.9. Implementando requisições condicionais com Deep ETags

--- src/main/java/com/algaworks/algafood/api/controller/FormaPagamentoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/FormaPagamentoController.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.api.controller;
 
+import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -18,6 +19,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
 import com.algaworks.algafood.api.assembler.FormaPagamentoInputDisassembler;
 import com.algaworks.algafood.api.assembler.FormaPagamentoModelAssembler;
@@ -44,8 +47,22 @@ public class FormaPagamentoController {
 	private FormaPagamentoInputDisassembler formaPagamentoInputDisassembler;
 
 	@GetMapping
-	public ResponseEntity<List<FormaPagamentoModel>> listar() {
+	public ResponseEntity<List<FormaPagamentoModel>> listar(ServletWebRequest request) {
+		ShallowEtagHeaderFilter.disableContentCaching(request.getRequest());
 		
+		String eTag = "0";
+		
+		OffsetDateTime dataUltimaAtualizacao = formaPagamentoRepository.getDataUltimaAtualizacao();
+		
+		if (dataUltimaAtualizacao != null)
+		{
+			eTag = String.valueOf(dataUltimaAtualizacao.toEpochSecond());
+		}	
+		// aqui já temos condições de saber se continua ou não o processamento
+		
+		if (request.checkNotModified(eTag)) {
+			return null;
+		}
 		
 		List<FormaPagamentoModel> formasPagamentoModel = 
 				formaPagamentoModelAssembler.toCollectionModel(formaPagamentoRepository.findAll());	
@@ -56,6 +73,7 @@ public class FormaPagamentoController {
 //				.cacheControl(CacheControl.noCache())  //ao fazer cache, validação da requisição passa a ser obrigatória sempre. não quer dizer que não há cache, como se a resposta em cache fosse sempre velha ( stale)
 //				.cacheControl(CacheControl.noStore()) // resposta não cacheável, nada será armazenado.
 				.cacheControl(CacheControl.maxAge(10, TimeUnit.SECONDS).cachePublic())
+				.eTag(eTag)
 				.body(formasPagamentoModel);
 	}
 
