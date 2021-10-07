cee45eb2e791070c4f8d70cace5450b594843ee9
Antonio Pires <tonan@globo.com>
2021-10-07 18:45:55 -0300

    19.7. Adicionando hypermedia na representação de recurso único com HAL

--- src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.IanaLinkRelations;
+import org.springframework.hateoas.Link;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -55,7 +57,17 @@ public class CidadeController implements CidadeControllerOpenApi {
 	public CidadeModel buscar(@PathVariable Long cidadeId){
 		Cidade cidade = cadastroCidadeService.buscarOufalhar(cidadeId);
 		
-		return cidadeModelAssembler.toModel(cidade);
+		CidadeModel cidadeModel = cidadeModelAssembler.toModel(cidade);
+		
+		cidadeModel.add(Link.of("https://localhost:8080/cidades/1"));
+//		cidadeModel.add(Link.of("https://localhost:8080/cidades/1", IanaLinkRelations.SELF));
+		
+//		cidadeModel.add(Link.of("https://localhost:8080/cidades", IanaLinkRelations.COLLECTION));
+		cidadeModel.add(Link.of("https://localhost:8080/cidades", "cidades"));
+		
+		cidadeModel.getEstado().add(Link.of("https://localhost:8080/estados/1"));
+		
+		return cidadeModel;
 	}
 	
 	@PostMapping
