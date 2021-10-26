76d98e01b09bd5254d28068af502f784d8f3d4c9
Antonio Pires <tonan@globo.com>
2021-10-26 07:08:52 -0300

    22.4. Configurando autenticação de usuários em memória

--- src/main/java/com/algaworks/algafood/core/security/WebSecurityConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/WebSecurityConfig.java
@@ -1,10 +1,14 @@
 package com.algaworks.algafood.core.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration //opcional, quando usada a anotação EnableWebSecurity
 @EnableWebSecurity
@@ -23,4 +27,25 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.and()
 				.csrf().disable();
 	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//		System.out.println(passwordEncoder().encode("123"));
+		
+		auth.inMemoryAuthentication()
+			.withUser("antonio")
+				.password(passwordEncoder().encode("123"))
+				.roles("ADMIN")
+			.and()
+			.withUser("joao")
+				.password(passwordEncoder().encode("123"))
+				.roles("ADMIN");
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	
 }
