0d470873f94c4f1fb79303a7523de52f09192874
Antonio Pires <tonan@globo.com>
2021-11-02 10:59:16 -0300

    23.15. Desafio: refatorando serviços de usuários para usar BCrypt

--- src/main/java/com/algaworks/algafood/domain/model/Usuario.java
+++ src/main/java/com/algaworks/algafood/domain/model/Usuario.java
@@ -46,15 +46,6 @@ public class Usuario {
 					inverseJoinColumns = @JoinColumn(name = "grupo_id"))
 	private Set<Grupo> grupos = new HashSet<>();
 	
-	
-	public boolean senhaCoincideCom(String senha) {
-		return getSenha().equals(senha);
-	}
-	
-	public boolean senhaNaoCoincideCom(String senha) {
-		return !senhaCoincideCom(senha);
-	}
-	
 	public boolean removerGrupo(Grupo grupo) {
 		return getGrupos().remove(grupo);
 	}
@@ -63,6 +54,17 @@ public class Usuario {
 		return getGrupos().add(grupo);
 	}
 	
+	public boolean isNovo() {
+		return getId() == null;
+	}
+	
+//	public boolean senhaCoincideCom(String senha) {
+//		return getSenha().equals(senha);
+//	}
+//	
+//	public boolean senhaNaoCoincideCom(String senha) {
+//		return !senhaCoincideCom(senha);
+//	}
 	
 	
 }
