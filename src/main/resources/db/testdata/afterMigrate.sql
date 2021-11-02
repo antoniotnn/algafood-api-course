4c1bc85a28ab2269bb43ce2c57496194fc28dec8
Antonio Pires <tonan@globo.com>
2021-11-02 14:05:56 -0300

    23.19. Carregando as permissões concedidas na autenticação

--- src/main/resources/db/testdata/afterMigrate.sql
+++ src/main/resources/db/testdata/afterMigrate.sql
@@ -122,7 +122,7 @@ insert into usuario (id, nome, email, senha, data_cadastro) values
 (6, 'Débora Mendonça', 'tonanpsn+debora.mendonca@gmail.com', '$2a$12$5lHBT6l99c.a//mZPXlNz.OrcbOcOpSu5TTLWqOukNzZoQeGD4r7a', utc_timestamp),
 (7, 'Carlos Lima', 'tonanpsn+carlos.lima@gmail.com', '$2a$12$5lHBT6l99c.a//mZPXlNz.OrcbOcOpSu5TTLWqOukNzZoQeGD4r7a', utc_timestamp);
 
-insert into usuario_grupo (usuario_id, grupo_id) values (1, 1), (1, 2), (2, 2);
+insert into usuario_grupo (usuario_id, grupo_id) values (1, 1), (1, 2), (2, 2), (3, 3), (4, 4);
 
 insert into restaurante_usuario_responsavel (restaurante_id, usuario_id) values (1, 5), (3, 5);
 
