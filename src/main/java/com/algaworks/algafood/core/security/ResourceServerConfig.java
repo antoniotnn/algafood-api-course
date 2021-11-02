e76fcb8288b3871174b739c198f8e6ed4afdfc33
Antonio Pires <tonan@globo.com>
2021-11-02 15:10:45 -0300

    23.20. Carregando as Granted Authorities e restringindo acesso a endpoints na API

--- src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
@@ -1,9 +1,15 @@
 package com.algaworks.algafood.core.security;
 
+import java.util.Collections;
+import java.util.stream.Collectors;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 
 @Configuration //opcional, quando usada a anotação EnableWebSecurity
 @EnableWebSecurity
@@ -13,12 +19,38 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
-				.anyRequest().authenticated()
+				.antMatchers(HttpMethod.POST, "/v1/cozinhas/**").hasAuthority("EDITAR_COZINHAS")
+				.antMatchers(HttpMethod.PUT, "/v1/cozinhas/**").hasAuthority("EDITAR_COZINHAS")
+				.antMatchers(HttpMethod.GET, "/v1/cozinhas/**").authenticated()
+				.anyRequest().denyAll()
+//				.anyRequest().authenticated()
 			.and()
 			.cors().and()
 //			.oauth2ResourceServer().opaqueToken();  para opaqueToken
-			.oauth2ResourceServer().jwt();    // para JWT
+			.oauth2ResourceServer()
+				.jwt()    // para JWT
+				.jwtAuthenticationConverter(jwtAuthenticationConverter());
 	}
+	
+	private JwtAuthenticationConverter jwtAuthenticationConverter() {
+		var jwtAuthenticationConverter = new JwtAuthenticationConverter();
+		jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwt -> {
+			var authorities = jwt.getClaimAsStringList("authorities");
+			
+			if (authorities == null) {
+				authorities = Collections.emptyList();
+			}
+			
+			return authorities.stream()
+					.map(SimpleGrantedAuthority::new)
+					.collect(Collectors.toList());
+		});
+		
+		return jwtAuthenticationConverter;
+	}
+	
+	
+	
 	/*
 	  
 	  CHAVE SIMÉTRICA
