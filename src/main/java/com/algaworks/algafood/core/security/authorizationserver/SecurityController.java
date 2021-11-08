bf2d437cdd0194e82b62249f29694d90b313e101
Antonio Pires <tonan@globo.com>
2021-11-08 07:20:31 -0300

    23.44. Customizando a página de OAuth2 Approval

--- src/main/java/com/algaworks/algafood/core/security/authorizationserver/SecurityController.java
+++ src/main/java/com/algaworks/algafood/core/security/authorizationserver/SecurityController.java
@@ -6,8 +6,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class SecurityController {
 	
+	
 	@GetMapping("/login")
 	public String login() {
-		return "pages/login";
+		return "pages/login";  // http://localhost:8080/oauth/authorize?response_type=code&client_id=foodanalytics&state=abc&redirect_uri=http://www.foodanalytics.local:8082
 	}
+	
+	//WhitelabelApprovalEndpoint classe referencia do sprint
+	
+	@GetMapping("/oauth/confirm_access")
+	public String approval() {
+		return "pages/approval";
+	}
+	
 }
